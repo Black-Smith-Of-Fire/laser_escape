2826d17fe10a2f5f374a15ed9221ab68a079f235
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-13 21:13:02 +0530

    Now using only the game class

--- src/main/java/org/blacksmith/Main.java
+++ src/main/java/org/blacksmith/Main.java
@@ -1,13 +1,8 @@
 package org.blacksmith;
 
-import javax.swing.*;
 
-public class Main extends JFrame {
+public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Escape the lasers");
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setVisible(true);
+        new Game();
     }
 }
