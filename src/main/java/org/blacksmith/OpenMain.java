7eaf13f772fc8d7695c7395c989401f36ae0f2d0
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-18 00:07:49 +0530

    lwjgl window is successful

--- src/main/java/org/blacksmith/OpenMain.java
+++ src/main/java/org/blacksmith/OpenMain.java
@@ -5,23 +5,7 @@ import org.lwjgl.glfw.*;
 import static org.lwjgl.glfw.GLFW.*;
 public class OpenMain {
     public static void main(String[] args) {
-        if (!glfwInit()) {
-            throw new IllegalStateException("Failed to initialize GLFW");
-        }
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        long window = glfwCreateWindow(640, 480,"First lwjgl", 0, 0);
-        if (window == 0) {
-            throw new IllegalStateException("Failed to create window");
-        }
-
-        GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        glfwSetWindowPos(window, ((videoMode.width() - 640) / 2), ((videoMode.height() - 480) / 2));
-
-        glfwShowWindow(window);
-
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-        glfwTerminate();
+        Window window = Window.get();
+        window.run();
     }
 }
