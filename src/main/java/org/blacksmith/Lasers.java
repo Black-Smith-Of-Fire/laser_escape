f52cd1b2e410f5b9298ba551dd31c2e20ec7916d
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-17 13:27:26 +0530

    Removed the collision code and relying on the move method only

--- src/main/java/org/blacksmith/Lasers.java
+++ src/main/java/org/blacksmith/Lasers.java
@@ -11,7 +11,6 @@ public class Lasers {
     int velX;
     int velY;
 
-    int collision;
     int boardWidth = 1860;
     int boardHeight = 1010;
 
@@ -21,58 +20,24 @@ public class Lasers {
         this.x = x;
         this.y = y;
 
-        setCollision(0);
         ball = new ImageIcon("characters/enemy/ball_obstacle.png").getImage();
     }
 
     public void move(){
-        if (y >= 0 && getCollision() == 0) {
-            x += 2;
-            y += 2;
-            setCollision(boardHeight);
+        if (x <= 0) {
+            velX = 2;
         }
-//        if (y >= boardHeight && getCollision() == boardHeight) {
-//            x += 2;
-//            y -= 2;
-//        }
+        if (y <= 0) {
+            velY = 2;
+        }
+        if (x >= boardWidth) {
+            velX = -2;
+        }
+        if (y >= boardHeight) {
+            velY = -2;
+        }
+        x += velX;
+        y += velY;
     }
 
-//    public void collision(){
-//        if (x >= boardWidth) {
-//            setX(-2);
-//        }
-//        if (y >= boardHeight) {
-//            setY(-2);
-//        }
-//        if (x == 0) {
-//            setX(2);
-//        }
-//        if (y == 0) {
-//            setY(2);
-//        }
-//    }
-
-    public void setX(int velX){
-        this.velX = velX;
-    }
-
-    public int getX(){
-        return velX;
-    }
-
-    public void setY(int velY){
-        this.velY = velY;
-    }
-
-    public int getY(){
-        return velY;
-    }
-
-    public void setCollision(int collision){
-        this.collision = collision;
-    }
-
-    public int getCollision(){
-        return collision;
-    }
 }
