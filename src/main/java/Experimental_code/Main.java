c3180a8b552c8257aa69b45a9e0a06cccf961846
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-18 15:34:53 +0530

    Experimenting with a new character

---
+++ src/main/java/Experimental_code/Main.java
@@ -0,0 +1,90 @@
+package Experimental_code;
+
+import org.blacksmith.Hero;
+import org.blacksmith.Lasers;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Main extends JFrame{
+
+    int x;
+    int y;
+    int width;
+    int height;
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public class lasers{
+
+        int x;
+        int y;
+
+        lasers(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    ArrayList<lasers> laserList;
+
+    Main(){
+
+        laserList = new ArrayList<>();
+        laserList.add(new lasers(50,50));//0
+        laserList.add(new lasers(100,50));//1
+        int lol = laserList.get(1).x;
+        laserList.add(new lasers(190,lol));//2
+//        laserList.add(new lasers(590,20));
+//        laserList.add(new lasers(590,50));
+//        laserList.add(new lasers(590,80));
+//        laserList.add(new lasers(590,70));
+//        laserList.add(new lasers(80,50));
+
+        width = 29;
+        height = 26;
+        JPanel panel = new JPanel();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Experimental code");
+        setExtendedState(getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        add(panel);
+        pack();
+        setVisible(true);
+    }
+
+
+    public void placeObstacles(){
+        for (int i = 0; i < 9; i++) {
+            int laserx = (laserList.get(i - 1).x + width)/2;
+            int lasery = (laserList.get(i - 1).y + height)/2;
+            laserList.get(i).x = laserx;
+            laserList.get(i).y = lasery;
+        }
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawString("Score : ", 1600,0);
+        draw((Graphics2D) g);
+    }
+
+    public void draw(Graphics2D g2d){
+        for (int i = laserList.size() - 1; i >= 0; i--) {
+            Image star = new ImageIcon("characters/enemy/starRed/redRect" + i +".png").getImage();
+            lasers laser = laserList.get(i);
+            System.out.println(i + " x pos : " + laser.x);
+            g2d.drawImage(star, laser.x, laser.y, width, height, null);
+        }
+//        g2d.drawImage(ball8, 520,520, width, height, null);
+//        g2d.drawImage(ball9, 520,520, width, height, null);
+//        g2d.drawImage(ball2, (x + width)/2,(y + height)/2, width, height, null);
+//        g2d.drawImage(ball1, x, y, width, height, null);
+    }
+
+//    public void
+}
