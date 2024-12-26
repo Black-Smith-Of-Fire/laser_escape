c289e119ecc7ec4db4f92daae2e27fe2d4b0b35e
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-26 22:36:56 +0530

    Trying to move one rect at a time

--- src/main/java/org/blacksmith/Lasers.java
+++ src/main/java/org/blacksmith/Lasers.java
@@ -26,7 +26,7 @@ public class Lasers{
 
         laserList = new ArrayList<>();
         laserList.add(new Items(x, y));
-        placeObstacles();
+//        placeObstacles();
 
         width = 29;
         height = 26;
@@ -34,10 +34,10 @@ public class Lasers{
 
 
     public void placeObstacles(){
-        for (int i = 1; i < 9; i++) {
-            laserList.add(new Items(laserList.get(i - 1).x + 14, laserList.get(i - 1).y - 14));//0
-//            System.out.println(i + " x : " + laserList.get(i).x);
-        }
+        laserList.add(new Items(laserList.get(1).x, laserList.get(1).y));
+//        for (int i = 1; i < 9; i++) {
+//            laserList.add(new Items(laserList.get(i - 1).x + 14, laserList.get(i - 1).y - 14));
+//        }
     }
 
     public void draw(Graphics2D g2d){
@@ -48,34 +48,34 @@ public class Lasers{
         }
     }
 
-    public void move(){
+    public void lol(Graphics2D g2d){
+        Image star = new ImageIcon("characters/enemy/starRed/redRect1.png").getImage();
+        g2d.drawImage(star, laserList.get(0).x, laserList.get(0).y, null);
+    }
+
+    public void moveObs(){
+        move(laserList.get(0).x, laserList.get(0).y); // God, let this work
+    }
+
+    public void move(int x, int y){
         // TODO : This for loop checks if each rectangle within the bounds .Afterwards it doesn't bother to
         // move it on
         for (int i = 0; i < 9; i++) {
-            if (laserList.get(i).x <= 0) {
+            if (x <= 0) {
                 velX = 2;
-//                System.out.println("x is " + laserList.get(1).x);
             }
 
-            if (laserList.get(i).y <= 0) {
+            if (y <= 0) {
                 velY = 2;
-//                System.out.println("y is " + laserList.get(1).y);
             }
 
-            if (laserList.get(i).x >= boardWidth) {
+            if (x >= boardWidth) {
                 velX = -2;
-//                System.out.println(" kkkkkkkkkkkkkkkklol x is " + laserList.get(1).x);
             }
 
-            if (laserList.get(i).y >= boardHeight) {
+            if (y >= boardHeight) {
                 velY = -2;
-//                System.out.println("lol lllllllllllllllllly is " + laserList.get(1).y);
             }
-            Items item = new Items(laserList.get(i).x + velX, laserList.get(i).y + velY);
-            System.out.println(i + " " + i + " " + i +" "+ i);
-            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX : " + item.x);
-            System.out.println("OOOOOOOOOOOOOOOOOO is : " + item.y);
-            laserList.set(i, item);
         }
     }
 
