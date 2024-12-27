ccc5a4ff2ab032d87506607e1ec8f1de76289ba0
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-27 19:12:38 +0530

    Adding velocities in move

--- src/main/java/org/blacksmith/Lasers.java
+++ src/main/java/org/blacksmith/Lasers.java
@@ -2,8 +2,6 @@ package org.blacksmith;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Lasers{
@@ -13,8 +11,9 @@ public class Lasers{
     int width;
     int height;
 
-    int velX = 0;
-    int velY = 0;
+    int velX;
+    int velY;
+
     int boardWidth = 1860;
     int boardHeight = 1010;
 
@@ -25,27 +24,34 @@ public class Lasers{
         this.y = y;
 
         laserList = new ArrayList<>();
-        laserList.add(new Items(x, y));
-//        placeObstacles();
+        laserList.add(new Items(x,y));
 
+        velX = 0;
+        velY = 0;
         width = 29;
         height = 26;
     }
 
 
-    public void placeObstacles(){
-        laserList.add(new Items(laserList.get(1).x, laserList.get(1).y));
+    public void placeObstacles()//        if(laserPosX == 520) {
+//            for (int i = 0; i < 9; i++) {
+//                lasers.laserList.get(i).x -= 2;
+//                lasers.laserList.get(i).y += 2;
+//            }
+//        }
+{
+//        laserList.add(new Items(laserList.get(1).x, laserList.get(1).y));
 //        for (int i = 1; i < 9; i++) {
 //            laserList.add(new Items(laserList.get(i - 1).x + 14, laserList.get(i - 1).y - 14));
 //        }
     }
 
     public void draw(Graphics2D g2d){
-        for (int i = laserList.size() - 1; i >= 0; i--) {
-            Image star = new ImageIcon("characters/enemy/starRed/redRect" + i +".png").getImage();
-            Items laser = laserList.get(i);
-            g2d.drawImage(star, laser.x, laser.y, width, height, null);
-        }
+//        for (int i = laserList.size() - 1; i >= 0; i--) {
+//            Image star = new ImageIcon("characters/enemy/starRed/redRect" + i +".png").getImage();
+//            Items laser = laserList.get(i);
+//            g2d.drawImage(star, laser.x, laser.y, width, height, null);
+//        }
     }
 
     public void lol(Graphics2D g2d){
@@ -54,29 +60,30 @@ public class Lasers{
     }
 
     public void moveObs(){
-        move(x, y); // God, let this work
+        move(); // This is sort of working
+        x += velX;
+        y += velY;
     }
 
-    public void move(int x, int y){
-        // TODO : This for loop checks if each rectangle within the bounds .Afterwards it doesn't bother to
-        // move it on
+    public void move(){
             if (x <= 0) {
-                velX = 2;
+                velX += 2;
+                System.out.println("This method is being used at : " + x);
             }
 
             if (y <= 0) {
-                velY = 2;
+                velY += 2;
+                System.out.println("This method is being used 000000000000 : " + y);
             }
 
             if (x >= boardWidth) {
-                velX = -2;
-            }
+                velX -= 2;
+             }
 
             if (y >= boardHeight) {
-                velY = -2;
+                velY -= 2;
+                System.out.println("This method is being used at : " + x);
             }
-        x += velX;
-        y += velY;
     }
 
 }
