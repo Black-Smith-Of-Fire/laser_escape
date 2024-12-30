e8321245a2dc9cfa969ea71c640c341c8fd4539b
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-30 22:27:53 +0530

    Refactored the code for multiple rects

--- src/main/java/org/blacksmith/MultipleLasers.java
+++ src/main/java/org/blacksmith/MultipleLasers.java
@@ -9,46 +9,48 @@ public class MultipleLasers {
     Lasers lasers = new Lasers(50,50);
 
     MultipleLasers(){
-//        laserList = new ArrayList<>();
-//        laserList.add(new Lasers(500,90));
-//        for (int i = 1; i < 9; i++) {
-//            laserList.add(new Lasers(laserList.get(i-1).x + 14,laserList.get(i-1).y + 13));
-//        }
+        laserList = new ArrayList<>();
+        laserList.add(new Lasers(500,90));
+        for (int i = 1; i < 9; i++) {
+            laserList.add(new Lasers(laserList.get(i-1).x + 14,laserList.get(i-1).y + 13));
+        }
     }
 
     public void lol(Graphics2D g2d){
         lasers.draw(g2d);
     }
 
-//    public void draw(Graphics2D g2d){
-//        for (int i = 0; i < laserList.size(); i++) {
-//            laserList.get(i).draw(g2d);
-//        }
-//    }
+    public void draw(Graphics2D g2d){
+        for (int i = 0; i < laserList.size(); i++) {
+            laserList.get(i).draw(g2d);
+        }
+    }
 
     public void push(){
-//        for (int i = 0; i < 9; i++) {
-//            laserList.get(i).x ++;
-//            laserList.get(i).y ++;
-//        }
-        lasers.x ++;
-        lasers.y ++;
+        for (int i = 0; i < 9; i++) {
+            laserList.get(i).x ++;
+            laserList.get(i).y ++;
+        }
+//        lasers.x ++;
+//        lasers.y ++;
     }
 
     public boolean collision(int x, int y){
-        if (x <= lasers.x &&
-                y <= lasers.y &&
-                (x + 200) > lasers.x &&
-                (y + 200) > lasers.y) {
-            return true;
+        for (int i = 0; i < 9; i++) {
+            if (x <= laserList.get(i).x &&
+                    y <= laserList.get(i).y &&
+                    (x + 200) > laserList.get(i).x &&
+                    (y + 200) > laserList.get(i).y) {
+                return true;
+            }
         }
         return false;
     }
 
     public void move(){
-//        for (int i = 0; i < 9; i++) {
-//            laserList.get(i).move();
-//        }
+        for (int i = 0; i < 9; i++) {
+            laserList.get(i).move();
+        }
         lasers.move();
     }
 }
