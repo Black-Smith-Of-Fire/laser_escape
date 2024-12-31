080c6a7909d4d78554706c93eb20291f8f26bc12
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-31 21:00:33 +0530

    Running Display class for now

--- src/main/java/org/blacksmith/Main.java
+++ src/main/java/org/blacksmith/Main.java
@@ -2,7 +2,10 @@ package org.blacksmith;
 
 public class Main {
     public static void main(String[] args) {
-        new Game();
+//        new Game();
 //        new Lasers(0,0);
+            DrawImageOnCavas game = new DrawImageOnCavas("draw image", 400, 400);
+            game.start();
+
     }
 }
