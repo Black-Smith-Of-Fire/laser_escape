4370eddf16c3db53ac1fc402444019e6d53874cf
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2025-01-01 11:42:36 +0530

    Shifted the wall collision function here

--- src/main/java/org/blacksmith/Hero.java
+++ src/main/java/org/blacksmith/Hero.java
@@ -11,14 +11,38 @@ public class Hero {
     Image worried;
     int x;
     int y;
+    int boardWidth;
+    int boardHeight;
+    int velX = 0;
+    int velY = 0;
 
     Hero(int x, int y){
         this.x = x;
         this.y = y;
 
+        boardWidth = 1750;
+        boardHeight = 940;
+
         happy = new ImageIcon("characters/hero/happy_hero.png").getImage();
         shocked = new ImageIcon("characters/hero/shocked_hero.png").getImage();
         worried = new ImageIcon("characters/hero/worried_hero.png").getImage();
 
+    }
+    public void wallCollision(){
+        if (x <= boardWidth && x >= 0) {
+            x += velX;
+        } else if (x <= 0) {
+            x += 1;
+        } else if (x >= boardWidth) {
+            x -= 1;
+        }
+
+        if (y <= boardHeight && y >= 0) {
+            y += velY;
+        } else if (y <= 0) {
+            y += 1;
+        } else if (y >= boardHeight) {
+            y -= 1;
+        }
     }
 }
