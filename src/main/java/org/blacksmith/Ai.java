441e0861061909b39778e0e7ea8cedffbea86cfa
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2025-01-02 23:58:40 +0530

    Implementing collision here

--- src/main/java/org/blacksmith/Ai.java
+++ src/main/java/org/blacksmith/Ai.java
@@ -11,6 +11,10 @@ public class Ai {
     int y;
     int xDir;
     int yDir;
+    int width = 179;
+    int height = 128;
+    int deduct = 0;
+    int scoreDeduct = 0;
 
     Ai(int x, int y, Image star){
         this.x = x;
@@ -41,5 +45,29 @@ public class Ai {
             y -= 1;
             yDir = 1;
         }
+    }
+
+    public boolean collision(int heroX, int heroY){
+            if (x >= heroX &&
+                    y >= heroY &&
+                    (heroX + width) > x &&
+                    (heroY + height) > y) {
+                System.out.println("Hurray");
+                return true;
+            }
+        return false;
+    }
+
+    public void score(int heroX, int heroY){
+
+        if (collision(heroX, heroY)) {
+            deduct = -50;
+            scoreDeduct = -10;
+        }
+        else {
+            deduct = 0;
+            scoreDeduct = 0;
+        }
+
     }
 }
