147c32232fa6491507e7855c24a847b0366b7726
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2025-04-15 22:40:59 +0530

    An abstract class to describe an object

---
+++ src/main/java/org/blacksmith/GameObject.java
@@ -0,0 +1,39 @@
+package org.blacksmith;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    protected int x,y;
+    protected ID id;
+    protected int velX, velY;
+
+    GameObject(int x, int y, ID id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+    public abstract void tick(); // TODO : find a way to do without this method
+    public abstract void render(Graphics g); // TODO: find a way to do without
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public void setId(ID id){
+        this.id = id;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public ID getId(){
+        return id;
+    }
+}
